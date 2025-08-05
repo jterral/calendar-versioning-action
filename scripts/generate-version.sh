6e800d5c262b6de30c72c2cdb60a405bfd1e89cb
jterral <jonathan.terral@outlook.com>
2025-08-05 21:40:06 +0200

    fix: inline script

--- scripts/generate-version.sh
+++ scripts/generate-version.sh
@@ -8,8 +8,8 @@ minor=$(git tag -l "$calver.*" | sort -V | wc -l)
 tag="${INPUT_TAG_PREFIX}${calver}.${minor}"
 version="${tag}+${GITHUB_RUN_NUMBER}"
 
-echo "tag=$tag" >> "$GITHUB_OUTPUT"
-echo "version=$version" >> "$GITHUB_OUTPUT"
+echo "tag=$tag" >> $GITHUB_OUTPUT
+echo "version=$version" >> $GITHUB_OUTPUT
 
 echo "Generated tag: $tag"
 echo "Generated version: $version"
