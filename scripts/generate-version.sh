b545199cd2d5977331453ef112b902e82221bcf3
Jonathan Terral <j.terral@betclicgroup.com>
2025-08-05 16:21:08 +0200

    feat: rework actions

---
+++ scripts/generate-version.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+echo "::group::🔖 Versioning"
+
+calver=$(date +'%y.%U')
+minor=$(git tag -l "$calver.*" | sort -V | wc -l)
+tag="${INPUT_TAG_PREFIX}${calver}.${minor}"
+version="${tag}+${GITHUB_RUN_NUMBER}"
+
+echo "tag=$tag" >> "$GITHUB_OUTPUT"
+echo "version=$version" >> "$GITHUB_OUTPUT"
+
+echo "Generated tag: $tag"
+echo "Generated version: $version"
+
+echo "::endgroup::"
---
+++ scripts/push-tag.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+echo "::group::🔧 Git Config"
+git config --global user.email "${INPUT_COMMIT_EMAIL}"
+git config --global user.name "${INPUT_COMMIT_NAME}"
+echo "::endgroup::"
+
+echo "::group::🏷️ Tagging"
+git tag -a "$TAG" -m "$VERSION"
+echo "Created tag $TAG"
+echo "::endgroup::"
+
+if [[ "$INPUT_PUSH_TAG" == "true" ]]; then
+  echo "::group::🚀 Pushing tag"
+  git push origin "$TAG" || {
+    echo "::error::❌ Failed to push tag '$TAG'. Check permissions or token scope."
+    exit 1
+  }
+  echo "✅ Tag $TAG pushed to origin"
+  echo "::endgroup::"
+else
+  echo "🧪 push-tag=false → skipping push of $TAG"
+fi
